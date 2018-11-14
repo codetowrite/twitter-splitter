199a7d49808a9d293de502bd77b3b0aa65ab74f5
Tim Hallyburton <tim.hallyburton@protonmail.com>
2018-11-14 13:40:32 +0100

    Added temporary system tray

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow} = require('electron') // imports for electron
+const {Menu, Tray} = require('electron') // imports for tray
 const path = require('path')
 const url = require('url')
 
 let window = null
+let tray = null;
 
 // Wait until the app is ready
 app.once('ready', () => {
+  app.isQuiting = false;
   // Create a new window
   window = new BrowserWindow({
     // Set the initial width to 800px
@@ -15,10 +18,25 @@ app.once('ready', () => {
     // Set the default background color of the window to match the CSS
     // background color of the page, this prevents any white flickering
     backgroundColor: "#D6D8DC",
+    icon: "res/icon.png",
     // Don't show the window until it's ready, this prevents any white flickering
     show: false,
     resizable: true
-  })
+  
+  });
+
+  tray = new Tray("res/icon.png");
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Show App', click:  function(){
+        window.show();
+    } },
+    { label: 'Quit', click:  function(){
+        app.isQuiting = true;
+        app.quit();
+    } }
+  ]);
+  tray.setToolTip('Twitter Splitter - ctw');
+  tray.setContextMenu(contextMenu);
   
 
   // Load a URL in the window to the local index.html path
@@ -32,4 +50,18 @@ app.once('ready', () => {
   window.once('ready-to-show', () => {
     window.show()
   })
+
+  window.on('minimize',function(event){
+    event.preventDefault();
+    window.hide();
+  });
+
+  window.on('close', function (event) {
+    if(!app.isQuiting){
+        event.preventDefault();
+        window.hide();
+    }
+
+    return false;
+  });
 })
